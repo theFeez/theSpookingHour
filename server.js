fb8562eac22e6664cebbc86dd0d6663ddbc1a23a
theFeez <zachfeez@knights.ucf.edu>
2017-06-03 20:08:13 -0400

    made blog page more friendly

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ app.use(express.static(__dirname+'/'));
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
+    console.log(process.env.mongoUrl);
 });
 
 app.listen(process.env.PORT||500,function(){
