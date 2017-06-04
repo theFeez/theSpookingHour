6e7e05497266d34bca16f17c0ac4d2da06a2c1cf
theFeez <zachfeez@knights.ucf.edu>
2017-06-04 04:12:10 -0400

    archive page layout done

--- server.js
+++ server.js
@@ -2,15 +2,29 @@ var express = require('express');
 var app = express();
 var url = process.env.mongoUrl;
 var mongoose = require('mongoose');
-mongoose.connect(process.env.mongoUrl);
+mongoose.connect('mongodb://theFeez:neonSlick@ds163301.mlab.com:63301/heroku_xpw4zd3n');
 var PostSchema = new mongoose.Schema({
     episode: Number,
-    name: String,
-    text: String
+    chrisTitle: String,
+    chrisText: String,
+    connorTitle: String,
+    connorText: String
 });
 var Post = mongoose.model('Post',PostSchema);
 app.use(express.static(__dirname+'/'));
 app.set('view engine', 'ejs');
+
+function readMore(string, maxWords) {
+    var strippedString = "<p>" + string + "</p>".trim();
+    var array = strippedString.split(" ");
+    var string = array.splice(0, maxWords).join(" ");
+
+    if(array.length > maxWords) {
+        string += "...";
+    }
+
+        return string ;
+}
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
@@ -19,20 +33,34 @@ app.get('/',function(req,res){
 app.get('/test/:episode/:name',function(req,res){
     var episode = req.params.episode;
     var name = req.params.name;
-    Post.find({'episode':episode,'name':name},function(err,data){
+    Post.find({'episode':episode},function(err,data){
         if(err){
             console.log(err);
             res.end();
         }
         else{
             console.log(data);
-            res.render('test',{post:data.text});
+
+            res.render('test',{post:data[0].text});
 
         }
     });
 
 
 });
+
+app.get('/blog',function(req,res){
+    Post.find().sort({episode:-1}).exec(function(err,data){
+        var docs = []
+        for(var i in data){
+
+            docs[i]={'episode':data[i].episode,'chrisTitle':data[i].chrisTitle,'chrisText':readMore(data[i].chrisText,90),'connorTitle':data[i].connorTitle,'connorText':readMore(data[i].connorText,90)};
+        }
+        res.render('blog',{posts:docs});
+    });
+
+})
+
 
 app.get('/posts/:episode/:name',function(req,res){
     var episode = req.params.episode;
