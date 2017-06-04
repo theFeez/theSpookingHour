d36e125cc196a24aec8e5279deedd370b4ba168a
theFeez <zachfeez@knights.ucf.edu>
2017-06-03 22:13:26 -0400

    playing with mongoose

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 var express = require('express');
 var app = express();
+var url = process.env.mongoUrl;
+var mongoose = require('mongoose');
+mongoose.connect(process.env.mongoUrl);
+var PostSchema = new mongoose.Schema({
+    episode: Number,
+    name: String,
+    text: String
+});
+var Post = mongoose.model('Post',PostSchema);
 app.use(express.static(__dirname+'/'));
 
 app.get('/',function(req,res){
-    
     res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/test',function(req,res){
+    Post.find(function(err,posts){
+        if(err){
+            console.log(err);
+            res.end;
+        }
+        else{
+            res.send(posts);
+        }
+    });
+
+
+});
+
+app.get('/posts/:episode/:name',function(req,res){
+    var episode = req.params.episode;
+    var name = req.params.name;
 
 });
 
 app.listen(process.env.PORT||500,function(){
-    console.log(process.env.mongoUrl);
+
     console.log('server listening');
 })
