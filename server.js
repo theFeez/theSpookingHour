efa183999616f8c71c7ef293db4157a46a1c4c35
theFeez <zachfeez@knights.ucf.edu>
2017-06-05 05:42:02 -0400

    added primitive comment features

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var app = express();
-//var config = require('./config');
+var config = require('./config');
 var url = process.env.mongoUrl;
 var mongoose = require('mongoose');
-mongoose.connect(process.env.mongoUrl);
-var PostSchema = new mongoose.Schema({
-    episode: Number,
-    chrisTitle: String,
-    chrisText: String,
-    connorTitle: String,
-    connorText: String
-});
-var Post = mongoose.model('Post',PostSchema);
+var bodyParser = require('body-parser');
+var xss = require('xss');
+mongoose.connect(config.mongoUrl);
+
+
+
 app.use(express.static(__dirname+'/'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true,limit:'50mb'}));
 
 function readMore(string, maxWords) {
     var strippedString = "<p>" + string + "</p>".trim();
@@ -26,6 +25,34 @@ function readMore(string, maxWords) {
 
         return string ;
 }
+
+function convertUTCDateToLocalDate(date) {
+    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+
+    var offset = date.getTimezoneOffset() / 60;
+    var hours = date.getHours();
+
+    newDate.setHours(hours - offset);
+
+    return newDate;
+}
+
+var PostSchema = new mongoose.Schema({
+    episode: Number,
+    chrisTitle: String,
+    chrisText: String,
+    connorTitle: String,
+    connorText: String
+});
+
+var CommentSchema = new mongoose.Schema({
+    id:Number,
+    time:{ type: Date, default:convertUTCDateToLocalDate(new Date())},
+    text:String
+});
+
+var Post = mongoose.model('Post',PostSchema);
+var Comment = mongoose.model('Comment',CommentSchema);
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/views/index.html');
@@ -59,20 +86,47 @@ app.get('/archive',function(req,res){
 app.get('/posts/:episode/:name',function(req,res){
     var episode = req.params.episode;
     var name = req.params.name;
-    if(name==='chris'){
-        Post.findOne({'episode':episode}).exec(function(err,data){
-            res.render('blogPost',{'title':data.chrisTitle,'name':name,'text':data.chrisText,'fullName':'Christopher Borges'});
-        });
-    }
-    else{
-        Post.findOne({'episode':episode}).exec(function(err,data){
-            res.render('blogPost',{'title':data.connorTitle,'name':name,'text':data.connorText,'fullName':'Connor Lloyd Falkner'});
-        });
-    }
+    var comments = [];
+    Comment.find().exec(function(err,data){
+        for(var i in data){
+            comments[i]={'text':xss(data[i].text)};
+        }
+        if(name==='chris'){
+            Post.findOne({'episode':episode}).exec(function(err,data){
+                if(err){
+                    console.log(err);
+                    res.end();
+                }
+                res.render('blogPost',{'title':data.chrisTitle,'name':name,'text':data.chrisText,'fullName':'Christopher Borges','comments':comments});
+            });
+        }
+        else{
+            Post.findOne({'episode':episode}).exec(function(err,data){
+                if(err){
+                    console.log(err);
+                    res.end();
+                }
+                res.render('blogPost',{'title':data.connorTitle,'name':name,'text':data.connorText,'fullName':'Connor Lloyd Falkner'});
+            });
+        }
+    });
+
 
 
 
 });
+
+app.post('/submitComment',function(req,res){
+    console.log(req.body);
+    var newComment = new Comment({'id':Math.floor(Math.random() * 1000000000),'text':req.body.comment});
+    newComment.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        res.end();
+    })
+
+})
 
 app.listen(process.env.PORT||500,function(){
 
