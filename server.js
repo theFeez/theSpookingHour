0ca7ec3ba7b514ec7d55ce0c8fe59be9b1cc1150
theFeez <zachfeez@knights.ucf.edu>
2017-06-05 21:01:21 -0400

    adjusted timestamp offset

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var url = process.env.mongoUrl;
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var xss = require('xss');
-mongoose.connect(process.env.mongoUrl);
+mongoose.connect(url);
 
 
 
@@ -25,6 +25,16 @@ function readMore(string, maxWords) {
 
         return string ;
 }
+function convertUTCDateToLocalDate(date) {
+ -    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+ -
+ -    var offset = date.getTimezoneOffset() / 60;
+ -    var hours = date.getHours();
+ -
+ -    newDate.setHours(hours - offset);
+ -
+ -    return newDate;
+ -}
 
 var PostSchema = new mongoose.Schema({
     episode: Number,
@@ -35,7 +45,7 @@ var PostSchema = new mongoose.Schema({
 
 var CommentSchema = new mongoose.Schema({
     id:Number,
-    time:{ type: Date, default:new Date()},
+    time:{ type: Date, default:convertUTCDateToLocalDate(new Date())},
     postEpisode:Number,
     postName:String,
     text:String
