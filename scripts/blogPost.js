caf7aa39d831b263e1f5f5d8611bf68bf3adf606
theFeez <zachfeez@knights.ucf.edu>
2017-06-07 18:21:14 -0400

    timestamp now correctly displays client local time

--- scripts/blogPost.js
+++ scripts/blogPost.js
@@ -1,28 +1,9 @@
-function convertUTCDateToLocalDate(date) {
-    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
-
-    var offset = date.getTimezoneOffset() / 60;
-    var hours = date.getHours();
-
-    newDate.setHours(hours - offset);
-
-    return newDate;
-}
-
 $(document).ready(function(){
     var arr = $('.timeStamp');
 
     for(var i=0; i<arr.length; i++){
-
         var oldDate = new Date(arr[i].innerText);
-        console.log(oldDate.getTimezoneOffset());
-        console.log(oldDate);
         var newDate = new Date(oldDate);
-        console.log(newDate);
-        //arr[i].innerText=newDate;
-
+        arr[i].innerText=newDate;
     }
-
-
-
 });
